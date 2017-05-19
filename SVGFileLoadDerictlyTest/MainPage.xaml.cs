f9f024835611ac2b73029c44db9c29a88dd5f2c2
Nico Zhu <v-zhumin@microsoft.com>
2017-05-19 15:58:02 +0800

    change

--- SVGFileLoadDerictlyTest/MainPage.xaml.cs
+++ SVGFileLoadDerictlyTest/MainPage.xaml.cs
@@ -32,33 +32,33 @@ namespace SVGFileLoadDerictlyTest
 
         private async void MyBtn_Click(object sender, RoutedEventArgs e)
         {
-            //using (HttpClient client = new HttpClient())
-            //{
-            //    var svg = new SvgImageSource();
-            //    try
-            //    {
-            //        var response = await client.GetAsync(new Uri("http://www.iconsvg.com/Home/downloadPicture?route=1333158662629220352&name=%E5%9C%86%E5%BD%A2%E7%9A%84_circular164"));
+            using (HttpClient client = new HttpClient())
+            {
+                var svg = new SvgImageSource();
+                try
+                {
+                    var response = await client.GetAsync(new Uri("http://www.iconsvg.com/Home/downloadPicture?route=1333158662629220352&name=%E5%9C%86%E5%BD%A2%E7%9A%84_circular164"));
 
-            //        if (response != null && response.StatusCode == HttpStatusCode.OK)
-            //        {
-            //            using (var stream =await response.Content.ReadAsStreamAsync())
-            //            {
-            //                using (var memStream = new MemoryStream())
-            //                {
-            //                    await stream.CopyToAsync(memStream);
-            //                    memStream.Position = 0;
-            //                    await  svg.SetSourceAsync(memStream.AsRandomAccessStream());
-            //                    MyImageView.Source = svg;
-            //                }
-            //            }
-            //        }
-            //    }
-            //    catch (Exception ex)
-            //    {
+                    if (response != null && response.StatusCode == HttpStatusCode.OK)
+                    {
+                        using (var stream = await response.Content.ReadAsStreamAsync())
+                        {
+                            using (var memStream = new MemoryStream())
+                            {
+                                await stream.CopyToAsync(memStream);
+                                memStream.Position = 0;
+                                await svg.SetSourceAsync(memStream.AsRandomAccessStream());
+                                MyImageView.Source = svg;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
 
-            //    }
+                }
             }
-
         }
     }
 }
+
